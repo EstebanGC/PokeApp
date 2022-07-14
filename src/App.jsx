7c2d3c0b1f005c96d98462dc954cb031f287641a
Esteban Carvajal <johan.gal.car@gmail.com>
2022-07-14 17:49:40 -0500

    Components and styles added. Part I finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react'
-import { getAllPokemons } from './services/pokemon'
+import { getAllPokemons, getPokemon } from './services/pokemon'
+import Navbar from './components/Navbar'
+import Card from './components/Card'
 import './App.css'
 
 function App() {
@@ -7,32 +9,71 @@ function App() {
   const [nextUrl, setNextUrl] = useState('')
   const [prevUrl, setPrevUrl] = useState('')
   const [loading, setLoading] = useState(true)
-  const initialUrl = 'https://pokeapi.co/api/v2/pokemon'
+  const initialUrl = "https://pokeapi.co/api/v2/pokemon?offset=40&limit=50"
   
   useEffect(() => {
     async function fetchData() {
       let response = await getAllPokemons(initialUrl);
       console.log(response)
       setNextUrl(response.next)
-      setPrevUrl(reponse.previous)
-      await loadingPokemon(response.results)
+      setPrevUrl(response.previous)
+      await loadPokemon(response.results)
       setLoading(false)
     }
     fetchData();
   }, [])
 
-  const loadingPokemon = async function (data) => {
-    let ipokemon = await Promise.all(data.map(async pokemon => {
-      let pokemonRecord = await getAllPokemons(pokemon);
-    }))
+  const next = async () => {
+    setLoading(true)
+    let data = await getAllPokemons(nextUrl)
+    await loadPokemon(data.results)
+    setNextUrl(data.next)
+    setPrevUrl(data.previous)
+    setLoading(false)
   }
 
+  const prev = async () => {
+    if(!prevUrl) 
+    return
+    setLoading(true)
+    let data = await getAllPokemons(prevUrl)
+    await loadPokemon(data.results)
+    setNextUrl(data.next)
+    setPrevUrl(data.previous)
+    setLoading(false)
+  }
+ 
+  const loadPokemon = async (data) => {
+    let iPokemonData = await Promise.all(data.map(async pokemon => {
+      let pokemonRecord = await getPokemon(pokemon.url)
+      return pokemonRecord
+    }))
+    setPokemonData(iPokemonData)
+  }
+
+  console.log(pokemonData)
+
   return (
+    <>
+    <Navbar/>
     <div>
-      { loading ? <h1>Loading ... </h1> : 
-        <h1>Data is fetched</h1>
-      }
+      { loading ? <h1>Loading ... </h1> : (
+        <>
+        <div className='btn'>
+          <button onClick={prev}>Prev</button>
+          <button onClick={next}>Next</button>
+        </div>
+        <div className='grid-container'>
+          {pokemonData.map((pokemon,i) => {
+            return <Card key={i} pokemon={pokemon}/>
+          })}
+        </div>
+        <button onClick={prev}>Prev</button>
+        <button onClick={next}>Next</button>
+        </>
+      )}
     </div>
+    </>
   )
 }
 
--- src/services/pokemon.js
+++ src/services/pokemon.js
@@ -1,3 +1,13 @@
+export async function getPokemon(url) {
+    return new Promise((resolve, reject) => {
+        fetch(url)
+        .then(res => res.json())
+        .then(data =>{
+            resolve(data);
+        })
+    })
+}
+
 export async function getAllPokemons(url) {
     return new Promise((resolve, reject) => {
         fetch(url).then(res => res.json())
@@ -6,3 +16,4 @@ export async function getAllPokemons(url) {
         })
     });
 }
+
