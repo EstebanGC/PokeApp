fc41a57b34e56b07729802426ac174a405096ef7
Esteban Carvajal <johan.gal.car@gmail.com>
2022-07-14 18:06:07 -0500

    some styles changed

--- src/components/helpers/typeColors.jsx
+++ src/components/helpers/typeColors.jsx
@@ -2,21 +2,21 @@ const typeColors = {
     bug: '#729f3f',
     dragon: '#53a4cf',
     fairy: '#fdb9e9',
-    fire: '#fd7d24',
+    fire: '#FF8825',
     ghost: '#7b62a3',
     ground: '#f7de3f',
-    normal: '#a4acaf',
-    pyschic: '#f366b9',
-    steel: '#9eb7b',
+    normal: '#000000',
+    psychic: '#4B446F',
+    steel: '#B3BBB4',
     dark: '#707070',
     electric: '#eed535',
     fighting: '#d56723',
-    flying: '#3dc7ef',
-    grass: '#9bcc50',
+    flying: '#62AADF',
+    grass: '#279034',
     ice: '#51c4e7',
-    poison: '#b97fc9',
-    rock: '#a38c21',
+    poison: '#5829A5 ',
+    rock: '#6A5122',
     water: '#4592c4'
 }
 
-export default typeColors;
+export default typeColors
